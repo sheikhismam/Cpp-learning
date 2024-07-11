cfea1f274ff14597c1af2e50afe837ea56b70125
sheikhismam <sheikhismam.bgctub@gmail.com>
2024-07-11 20:03:02 +0600

    Add files via upload

---
+++ data_type_variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Girraffe Academy";
+    int rollNo = 35;
+    double gpa = 3.9;
+
+    cout << grade << endl;
+    cout << phrase << endl;
+    cout << rollNo << endl;
+    cout << gpa << endl;
+    cout << "At a glance : roll no. = " << rollNo << ", grade = " << grade << ", gpa = " << gpa << ", phrase = " << phrase <<  endl; 
+
+    return 0;
+}
