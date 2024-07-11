c6038ebbfa0e01d5e27778225b60625292ff8c6f
sheikhismam <sheikhismam.bgctub@gmail.com>
2024-07-11 22:44:22 +0600

    using operators directly in printing statement.

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //addition. Subtraction, multiplication, division are same as addiion.
+    //There is another operator, though, called modulus (%) which prints the remainder.
+    cout << 5+7 << endl;
+
+    //Order of operation
+    /* It basically follows the PEMDAS which means Parentheses, 
+    Exponent, Multiplication, Division, Addition, Subtraction.
+    */
+    cout << (5+7)-1*2/3 << endl;
+    return 0;
+}
