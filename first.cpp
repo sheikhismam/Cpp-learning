a0309a758578888db215ad9dcb57817058aa4f05
sheikhismam <sheikhismam.bgctub@gmail.com>
2024-07-11 16:23:29 +0600

    My first code in c++.

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string characterName = "John";
+    int characterAge = 35;
+    cout << "There was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old." << endl;
+    cout << "He liked the name " << characterName  << endl;
+    cout << "But he did not liked being " << characterAge << "." << endl;
+    
+    return 0;
+}
