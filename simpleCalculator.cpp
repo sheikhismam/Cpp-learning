09ffa67567e8d899522ee56c86a5c02ba2a18daf
sheikhismam <sheikhismam.bgctub@gmail.com>
2024-07-12 16:46:18 +0600

    A simple calculator for adding numbers

---
+++ simpleCalculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter two numbers." << endl;
+    
+    cout << "Enter first numbers: ";
+    cin >> num1;
+
+    cout << "Enter second numbers: ";
+    cin >> num2;
+
+    cout << "Sum is: " << num1 + num2 << endl;
+
+    return 0;
+}
