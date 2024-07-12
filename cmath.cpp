d7756f40dbffd80b3227286de7f82e09d6ffa1de
sheikhismam <sheikhismam.bgctub@gmail.com>
2024-07-12 16:44:57 +0600

    About some math functions

---
+++ cmath.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    //Some pretty basic function that come handy in c++. Not all of them are covered here.
+    //Power function
+    cout << pow(2,2) << endl;
+    //Round function
+    cout << round(3.2) << endl;
+    //Ceil function
+    cout << ceil(3.2) << endl;
+    //Floor function
+    cout << floor(4.5) << endl;
+    //Square root function 
+    cout << sqrt(36) << endl;
+    //Maximum function. Takes two numbers and tells which one is bigger.
+    cout << fmax(4,5) << endl;
+    //Minimum function. Takes two numbers and tells which is smaller.
+    cout << fmin(4,5) << endl;
+    //there are so many C plus plus math function. Go google and learn them.
+    return 0;
+}
