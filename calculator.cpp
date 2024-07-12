3455efe0f52234e05207eb123e021c849267be28
sheikhismam <sheikhismam.bgctub@gmail.com>
2024-07-12 21:53:57 +0600

    A very simple calculator to do pretty basic math

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator: ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    int result;
+    if(op == '+'){
+        result = num1 + num2;
+    } else if(op == '-'){
+        result = num1 - num2;
+    } else if(op == '*'){
+        result = num1 * num2;
+    } else if(op == '/'){
+        result = num1 / num2;
+    } else {
+        cout << "Invalid input\n";
+    }
+
+    cout << result;
+    return 0;
+}
